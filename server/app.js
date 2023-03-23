694f70c41b5c0c6402c022a8cb684bff6e2db9cd
Kevin Hoang <knhoangre@gmail.com>
2023-03-22 20:08:07 -0400

    Completed the app... Next step: Stress test

--- server/app.js
+++ server/app.js
@@ -2,6 +2,49 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const dbQuery = require('../database/dbQuery.ts');
+const axios = require('axios');
+var cors = require('cors');
+const path = require('path');
+
+
+app.use(express.json());
+app.use(express.urlencoded());
+// app.use(bodyParser.json());
+app.use(cors());
+// app.use(express.static(path.join(__dirname,'../../FECv2/dist')));
+app.use('/:product_id', express.static(path.join(__dirname,'../../FECv2/dist')));
+
+
+app.get('/:product_id', (req, res) => {
+  console.log(req.params, req.query)
+  // axios.get(`http://localhost:3000/products/${req.params.product_id}`, (err, data) => {
+  //   if (data) {
+  //     res.status(200).send(data.data);
+  //   } else {
+  //     res.status(500).send(err)
+  //   }
+  // })
+  dbQuery.getProductId(req.params.product_id, (err, data) => {
+    if (data) {
+      console.log('data', data)
+      res.status(200).send(data);
+    } else {
+      res.status(500).send(err);
+    }
+  });
+});
+
+app.post('/products', (req, res) => {
+  const product_id = req.body.product_id;
+  dbQuery.getProductId(product_id, (err, data) => {
+    if (data) {
+      // console.log('data', data)
+      res.status(200).send(data);
+    } else {
+      res.status(500).send(err);
+    }
+  });
+})
 
 app.get('/products', (req, res) => {
   let page;
@@ -18,9 +61,9 @@ app.get('/products', (req, res) => {
 });
 
 app.get('/products/:product_id', (req, res) => {
-  console.log(req.params)
   dbQuery.getProductId(req.params.product_id, (err, data) => {
     if (data) {
+      // console.log('data', data)
       res.status(200).send(data);
     } else {
       res.status(500).send(err);
@@ -30,6 +73,7 @@ app.get('/products/:product_id', (req, res) => {
 
 app.get('/products/:product_id/styles', (req, res) => {
   dbQuery.getStyle(req.params.product_id, (err, data) => {
+    console.log(data)
     if (data) {
       res.status(200).send(data);
     } else {
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const app = require('./app');
 
+
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`App listening on port ${process.env.SERVER_PORT}`)
 });
